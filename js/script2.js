b0295c1a40aedd7726f11c7a6a7fe697b38fa07e
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-11-13 20:58:29 +0100

    actualizado

--- js/script2.js
+++ js/script2.js
@@ -3,7 +3,9 @@ let mostrarCorreo = document.getElementById("correoMostrado");
 let verificaFecha = document.getElementById("verificacion");
 let boton = document.getElementById("botonPreguntas");
 
-//document.addEventListener("DOMContentLoaded", () => {
+
+
+document.addEventListener("DOMContentLoaded", () => {
 const correo = localStorage.getItem("correo");
 const mostrarFecha = localStorage.getItem("fechaGuardada");
 
@@ -13,7 +15,7 @@ const fecha = document.getElementById("verificacion");
 p.textContent = "Correo guardado: " + correo;
 fecha.textContent = "La ulitma vez que entraste fue el " + mostrarFecha;
 
-//});
+});
 
 boton.addEventListener("click", () => {
   window.location.href = "pantalla3.html";
---
+++ js/script3.js
@@ -0,0 +1,13 @@
+
+
+//Variables para hacer los addeventlistener
+let introducePregunta= document.getElementById("pregunta");
+let botonVerdadero= document.getElementById("verdadero");
+let botonFalso= document.getElementById("falso");
+let listPreguntas= document.getElementById();
+
+//funciones addEventListener
+
+
+
+
