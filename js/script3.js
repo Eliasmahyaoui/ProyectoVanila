fedd373cb6ea14507b2f6a93bb0680da2038b4ad
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-11-13 21:20:12 +0100

    actualizado

--- js/script3.js
+++ js/script3.js
@@ -3,10 +3,18 @@ let introducePregunta = document.getElementById("pregunta");
 let botonVerdadero = document.getElementById("verdadero");
 let botonFalso = document.getElementById("falso");
 let atras = document.getElementById("atras");
+let puntuacion = document.getElementById("puntuacion");
 //let listPreguntas = document.getElementById();
 
 //funciones addEventListener
 
 atras.addEventListener("click", () => {
   window.location.href = "pantalla2.html";
+});
+
+puntuacion.addEventListener("blur", () => {
+ 
+  if (puntuacion.value) {
+     console.log(puntuacion.value);
+  }
 });
