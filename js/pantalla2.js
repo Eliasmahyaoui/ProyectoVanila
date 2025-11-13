891779bad1f2ce412266add676410d271cf109f7
Eliasmahyaoui <eliasmahyaoui6@hotmail.com>
2025-11-13 17:50:16 +0100

    actualizado

--- js/pantalla2.js
+++ js/pantalla2.js
@@ -1,14 +1,20 @@
 //Estas variables seran para la pantalla 2
 let mostrarCorreo = document.getElementById("correoMostrado");
 let verificaFecha = document.getElementById("verificacion");
+let boton = document.getElementById("botonPreguntas");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const correo = localStorage.getItem("correo");
-  const mostrarFecha = localStorage.getItem("fechaGuardada");
+//document.addEventListener("DOMContentLoaded", () => {
+const correo = localStorage.getItem("correo");
+const mostrarFecha = localStorage.getItem("fechaGuardada");
 
-  const p = document.getElementById("correoMostrado");
-  const fecha = document.getElementById("verificacion");
+const p = document.getElementById("correoMostrado");
+const fecha = document.getElementById("verificacion");
 
-  p.textContent = "Correo guardado: " + correo;
-  fecha.textContent = "La ulitma vez que entraste fue el " + mostrarFecha;
+p.textContent = "Correo guardado: " + correo;
+fecha.textContent = "La ulitma vez que entraste fue el " + mostrarFecha;
+
+//});
+
+boton.addEventListener("click", () => {
+  window.location.href = "pantalla3.html";
 });
